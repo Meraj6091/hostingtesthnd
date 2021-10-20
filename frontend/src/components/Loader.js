68a701fb28c781fce06d03625b70fa2138378c7e
meraj6091 <merajvindira2000@gmail.com>
2021-10-20 19:42:33 +0530

    donr

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap'
+import React from "react";
+import { Spinner } from "react-bootstrap";
 const Loader = () => {
-    return (
-        <center><Spinner 
-            animation='border' 
-            role='status'
-            style ={{
-                width: '100px',
-                height: '100px',
-                margin: 'autp',
-                display: 'block',
-            }}
-        >
-            <span className='sr-only'>Loading....</span>
-        </Spinner>
-        </center>
-    )
-}
+  return (
+    <center>
+      <Spinner
+        animation="border"
+        role="status"
+        style={{
+          width: "100px",
+          height: "100px",
+          margin: "autp",
+          display: "block",
+        }}
+      >
+        <span className="sr-only">Loading....</span>
+      </Spinner>
+    </center>
+  );
+};
 
-export default Loader
+export default Loader;
--- frontend/src/screens/AllProducts.js
+++ frontend/src/screens/AllProducts.js
@@ -1,38 +1,40 @@
-import React, {useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import ProductCarousel from '../components/ProductCarousel'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import ProductCarousel from "../components/ProductCarousel";
 // import Meta from '../components/Meta'
-import { listProducts } from '../actions/productActions'
+import { listProducts } from "../actions/productActions";
 
 const AllProducts = ({ match }) => {
-  const keyword = match.params.keyword
+  const keyword = match.params.keyword;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const productList = useSelector((state) => state.productList)
-  const { loading, error, products, page, pages } = productList
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
 
   useEffect(() => {
-    dispatch(listProducts(keyword))
-  }, [dispatch, keyword])
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
-    <>  
-      {(
-        <Link to='/allproducts' className='btn btn-light'>
+    <>
+      {
+        <Link to="/allproducts" className="btn btn-light">
           Go Back
         </Link>
-      )}
-      <h1><center>All Products</center></h1>
+      }
+      <h1>
+        <center>All Products</center>
+      </h1>
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant="danger">{error}</Message>
       ) : (
         <>
           <Row>
@@ -42,11 +44,10 @@ const AllProducts = ({ match }) => {
               </Col>
             ))}
           </Row>
-      
         </>
       )}
     </>
-  )
-}
+  );
+};
 
-export default AllProducts
+export default AllProducts;
